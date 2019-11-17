d60b3920cf7f94926d1966311a261ce6864f4b5c
wim.henderickx@gmail.com <wim.henderickx@gmail.com>
2019-11-17 13:41:28 +0100

    output tap additions

--- outputTap.go
+++ outputTap.go
@@ -139,8 +139,10 @@ func (t *tapOutput) tapOutputLoop() {
 			fmt.Printf("Tap module raw data received: %#v \n", dMsg)
 			d := dMsg.getDataMsgBody()
 			fmt.Printf("Tap module raw data received after function: %#v \n", *d)
+			m := &dMsgBody{}
+			m = d
 			//js, _ := json.MarshalIndent(&*d, "", "  ")
-			js, err := json.Marshal(*d)
+			js, err := json.Marshal(m)
 			if err != nil {
 				fmt.Printf("Marshal issue: %s \n", err)
 			}
