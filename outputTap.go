3a237ce741d5be15b0ff503418eb01fd40a72a47
wim.henderickx@gmail.com <wim.henderickx@gmail.com>
2019-11-17 13:05:11 +0100

    adding tap output module

--- outputTap.go
+++ outputTap.go
@@ -139,7 +139,7 @@ func (t *tapOutput) tapOutputLoop() {
 			fmt.Printf("Tap module raw data received: %#v \n", dMsg)
 			d := dMsg.getDataMsgBody()
 			fmt.Printf("Tap module raw data received after function: %#v \n", *d)
-			js, _ := json.MarshalIndent(&d, "", "  ")
+			js, _ := json.MarshalIndent(*d, "", "  ")
 			fmt.Printf("Tap module data received: %s \n", js)
 			w.WriteString(string(js))
 			//w.WriteString(out.String())
