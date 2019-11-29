b7cfd4da0c11244af9982d198aa9d3cc42a3227c
wim.henderickx@gmail.com <wim.henderickx@gmail.com>
2019-11-29 17:00:53 +0100

    adding output metrics influxdb

--- outputMetricsInflux.go
+++ outputMetricsInflux.go
@@ -129,7 +129,7 @@ func (w *metricsInfluxOutputWorker) worker(m *metricsOutput) {
 				fields[u] = i
 			}
 
-			pt, err := client.NewPoint("interface_stats", tags, fields, time.Unix(0, data.Timestamp*int64(time.Millisecond)))
+			pt, err := client.NewPoint("interface_stats", tags, fields, time.Unix(0, data.Timestamp*int64(time.Nanosecond)))
 
 			bp.AddPoint(pt)
 
